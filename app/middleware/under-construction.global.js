6b65292ac17a80a1b79e393268c5f3abe29222c7
Alexander Matte <alex.matte71@gmail.com>
2025-05-28 21:48:32 +0200

    Removed middleware guard for production

--- app/middleware/under-construction.global.js
+++ app/middleware/under-construction.global.js
@@ -1,7 +1,7 @@
 export default defineNuxtRouteMiddleware((to) => {
-  if (process.env.VERCEL_ENV !== 'production') return
+/*   if (process.env.VERCEL_ENV !== 'production') return
 
   if (to.path !== '/under-construction') {
     return navigateTo('/under-construction')
-  }
+  } */
 })
